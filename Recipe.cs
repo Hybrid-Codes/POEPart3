10b0535ffc343cd077e2f261afc2afa530c6cb7d
Robert Burston <ST10086237@imconnect.edu.za>
2023-07-04 13:59:51 +0200

    Recipe class
    Recipe class added with all required logic in the code, this includes fields and objects of Recipe and Ingredient.

---
+++ Recipe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace POEPart3
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
+        public Recipe()
+        {
+            Ingredients = new List<Ingredient>();
+        }
+    }
+
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public string Measurement { get; set; }
+        public int Quantity { get; set; }
+        public int Calories { get; set; }
+        public string FoodGroup { get; set; }
+        
+    }
+}
