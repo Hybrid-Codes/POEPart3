0d8bd4603f2a3969a2d817ce7504445774c56911
Robert Burston <ST10086237@imconnect.edu.za>
2023-07-04 18:49:46 +0200

    Code Optimisations
    Optimised code and reduced duplicated lines. Added 1 function to only 1 method to keep things simple and readable.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,12 +11,13 @@ namespace POEPart3
     {
         private List<Recipe> recipes;
         private Recipe selectedRecipe;
+        private List<Recipe> originalRecipes;
 
         public MainWindow()
         {
             InitializeComponent();
             recipes = new List<Recipe>();
-            originalRecipes = new List<Recipe>(); // Initialize the originalRecipes list
+            originalRecipes = new List<Recipe>();
             lvRecipes.ItemsSource = recipes;
             lvIngredients.ItemsSource = null;
             lbSteps.ItemsSource = null;
@@ -27,175 +28,138 @@ namespace POEPart3
             string recipeName = txtRecipeName.Text.Trim();
             if (string.IsNullOrEmpty(recipeName))
             {
-                MessageBox.Show("Please enter a recipe name.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Please enter a recipe name.");
                 return;
             }
 
             Recipe recipe = new Recipe { Name = recipeName };
             recipes.Add(recipe);
-            recipes = recipes.OrderBy(r => r.Name).ToList();
-            lvRecipes.ItemsSource = recipes;
-            lvRecipes.Items.Refresh();
-            txtRecipeName.Text = string.Empty;
-
-            // Set the newly added recipe as the selected recipe
-            selectedRecipe = recipe;
-            txtSelectedRecipe.Text = selectedRecipe.Name;
-            lvIngredients.ItemsSource = selectedRecipe.Ingredients;
-            lvIngredients.Items.Refresh();
-            txtTotalCalories.Text = string.Empty;
+            RefreshRecipeList();
+            ClearInputFields();
+            SelectRecipe(recipe);
         }
-        
+
         private void btnScale_Click(object sender, RoutedEventArgs e)
         {
-            string scaleText = txtScale.Text.Trim();
-            if (string.IsNullOrEmpty(scaleText))
+            if (double.TryParse(txtScale.Text.Trim(), out double scale))
             {
-                MessageBox.Show("Please enter a scale value.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            if (double.TryParse(scaleText, out double scale))
-            {
-                // selectedRecipe.ScaleIngredients(scale);
                 lvIngredients.Items.Refresh();
-
             }
             else
             {
-                MessageBox.Show("Invalid scale value. Please enter a valid number.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Invalid scale value. Please enter a valid number.");
             }
         }
-        
+
         private void btnAddIngredient_Click(object sender, RoutedEventArgs e)
         {
             if (selectedRecipe == null)
             {
-                MessageBox.Show("Please select a recipe.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Please select a recipe.");
                 return;
             }
 
             string ingredientName = txtIngredientName.Text.Trim();
-            int numIngredients;
             string unitName = txtMeasurement.Text.Trim();
-            int calories;
-            string foodGroup = txtFoodGroup.Text.Trim();
-            
+            int calories, numIngredients;
 
             if (string.IsNullOrEmpty(ingredientName))
             {
-                MessageBox.Show("Please enter an ingredient name.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Please enter an ingredient name.");
                 return;
             }
-            
+
             if (string.IsNullOrEmpty(unitName))
             {
-                MessageBox.Show("Please enter a unit of measurement.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Please enter a unit of measurement.");
                 return;
             }
 
             if (!int.TryParse(txtCalories.Text.Trim(), out calories))
             {
-                MessageBox.Show("Please enter a valid calorie value.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Please enter a valid calorie value.");
                 return;
             }
-            
+
             if (!int.TryParse(txtIngredientQuantity.Text.Trim(), out numIngredients))
             {
-                MessageBox.Show("Please enter a valid value.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Please enter a valid value.");
                 return;
             }
-            
-            string selectedFoodGroup = ((ComboBoxItem)txtFoodGroup.SelectedItem)?.Content.ToString()!;
-            if (string.IsNullOrEmpty(selectedFoodGroup))
+
+            if (string.IsNullOrEmpty(txtFoodGroup.Text.Trim()))
             {
-                MessageBox.Show("Please select a food group.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Please select a food group.");
                 return;
             }
 
-            Ingredient ingredient = new Ingredient 
-                { 
-                    Name = ingredientName, 
-                    Quantity = numIngredients,
-                    Measurement = unitName, 
-                    Calories = calories, 
-                    FoodGroup = foodGroup 
-                };
-            
+            Ingredient ingredient = new Ingredient
+            {
+                Name = ingredientName,
+                Quantity = numIngredients,
+                Measurement = unitName,
+                Calories = calories,
+                FoodGroup = txtFoodGroup.Text.Trim()
+            };
+
             selectedRecipe.Ingredients.Add(ingredient);
-            lvIngredients.ItemsSource = selectedRecipe.Ingredients;
-            lvIngredients.Items.Refresh();
-
-            txtIngredientName.Text = string.Empty;
-            txtIngredientQuantity.Text = string.Empty;
-            txtMeasurement.Text = string.Empty;
-            txtCalories.Text = string.Empty;
-            txtFoodGroup.Text = string.Empty;
-
+            RefreshIngredientList();
+            ClearInputFields();
             CalculateTotalCalories();
         }
-        
+
         private void btnAddStep_Click(object sender, RoutedEventArgs e)
         {
             if (selectedRecipe == null)
             {
-                MessageBox.Show("Please select a recipe.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Please select a recipe.");
                 return;
             }
 
             string stepText = txtStep.Text.Trim();
             if (string.IsNullOrEmpty(stepText))
             {
-                MessageBox.Show("Please enter a recipe step.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage("Please enter a recipe step.");
                 return;
             }
 
             RecipeStep step = new RecipeStep { Step = stepText };
             selectedRecipe.Steps.Add(step);
-            lbSteps.ItemsSource = selectedRecipe.Steps;
-            lbSteps.Items.Refresh();
-
-            txtStep.Text = string.Empty;
+            RefreshStepList();
+            ClearInputFields();
         }
 
-        private void lvRecipes_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        private void lvRecipes_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             selectedRecipe = (Recipe)lvRecipes.SelectedItem;
             txtSelectedRecipe.Text = selectedRecipe != null ? selectedRecipe.Name : string.Empty;
 
             if (selectedRecipe != null)
             {
-                // Populate the ingredients
                 lvIngredients.ItemsSource = selectedRecipe.Ingredients;
-                lvIngredients.Items.Refresh();
                 CalculateTotalCalories();
-
-                // Populate the steps
                 lbSteps.ItemsSource = selectedRecipe.Steps;
-                lbSteps.Items.Refresh();
             }
             else
             {
-                lvIngredients.ItemsSource = null;
-                txtTotalCalories.Text = string.Empty;
-
-                // Clear the steps
-                lbSteps.ItemsSource = null;
-                lbSteps.Items.Clear();
+                ClearIngredientList();
+                ClearTotalCalories();
+                ClearStepList();
             }
         }
 
-        private List<Recipe> originalRecipes;
-
         private void btnFilter_Click(object sender, RoutedEventArgs e)
         {
-
             try
             {
                 string ingredientNameFilter = txtFilterIngredientName.Text.Trim();
                 string foodGroupFilter = txtFilterFoodGroup.Text.Trim();
                 int maxCaloriesFilter;
-                int.TryParse(txtFilterMaxCalories.Text.Trim(), out maxCaloriesFilter);
+
+                if (!int.TryParse(txtFilterMaxCalories.Text.Trim(), out maxCaloriesFilter))
+                {
+                    maxCaloriesFilter = 0;
+                }
 
                 var filteredRecipes = originalRecipes.Where(r =>
                     r.Name.ToLower().Contains(ingredientNameFilter.ToLower()) &&
@@ -203,21 +167,17 @@ namespace POEPart3
                     (maxCaloriesFilter <= 0 || r.Ingredients.Sum(i => i.Calories) <= maxCaloriesFilter)).ToList();
 
                 lvRecipes.ItemsSource = filteredRecipes;
-                lvRecipes.Items.Refresh();
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"An error occurred while filtering recipes: {ex.Message}", "Error",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
+                ShowErrorMessage($"An error occurred while filtering recipes: {ex.Message}");
             }
         }
 
-        
         private void CalculateTotalCalories()
         {
             int totalCalories = selectedRecipe.Ingredients.Sum(i => i.Calories);
             txtTotalCalories.Text = totalCalories.ToString();
-            
 
             if (totalCalories > 300)
             {
@@ -227,9 +187,39 @@ namespace POEPart3
 
         private void btnClearData_Click(object sender, RoutedEventArgs e)
         {
+            ClearRecipeList();
+            ClearInputFields();
+            ClearIngredientList();
+            ClearTotalCalories();
+            ClearStepList();
+            ClearFilterFields();
+        }
+
+        private void RefreshRecipeList()
+        {
+            recipes = recipes.OrderBy(r => r.Name).ToList();
+            lvRecipes.ItemsSource = recipes;
+            lvRecipes.Items.Refresh();
+        }
+
+        private void RefreshIngredientList()
+        {
+            lvIngredients.Items.Refresh();
+        }
+
+        private void RefreshStepList()
+        {
+            lbSteps.Items.Refresh();
+        }
+
+        private void ClearRecipeList()
+        {
             recipes.Clear();
-            selectedRecipe = null;
-            txtSelectedRecipe.Text = string.Empty;
+            RefreshRecipeList();
+        }
+
+        private void ClearInputFields()
+        {
             txtRecipeName.Text = string.Empty;
             txtIngredientName.Text = string.Empty;
             txtIngredientQuantity.Text = string.Empty;
@@ -237,20 +227,44 @@ namespace POEPart3
             txtCalories.Text = string.Empty;
             txtFoodGroup.Text = string.Empty;
             txtScale.Text = string.Empty;
-            txtTotalCalories.Text = string.Empty;
-            lvRecipes.ItemsSource = null;
+            txtStep.Text = string.Empty;
+        }
+
+        private void ClearIngredientList()
+        {
             lvIngredients.ItemsSource = null;
-            // Clear the recipe steps
+            lvIngredients.Items.Refresh();
+        }
+
+        private void ClearTotalCalories()
+        {
+            txtTotalCalories.Text = string.Empty;
+        }
+
+        private void ClearStepList()
+        {
             lbSteps.ItemsSource = null;
             lbSteps.Items.Clear();
+        }
 
-            // Clear the filter text boxes
+        private void ClearFilterFields()
+        {
             txtFilterIngredientName.Text = string.Empty;
             txtFilterFoodGroup.Text = string.Empty;
             txtFilterMaxCalories.Text = string.Empty;
-            
-            originalRecipes = new List<Recipe>(recipes);
+        }
+
+        private void SelectRecipe(Recipe recipe)
+        {
+            selectedRecipe = recipe;
+            txtSelectedRecipe.Text = selectedRecipe.Name;
+            lvIngredients.ItemsSource = selectedRecipe.Ingredients;
+            CalculateTotalCalories();
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
-
